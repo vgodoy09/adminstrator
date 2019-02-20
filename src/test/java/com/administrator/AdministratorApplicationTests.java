42c1f064c527d114abaa49963a3730d9e17a9526
victor.godoy <victor.godoy@vmengsoft02>
2019-02-20 17:36:26 -0300

    ..

---
+++ src/test/java/com/administrator/AdministratorApplicationTests.java
@@ -0,0 +1,16 @@
+package com.administrator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdministratorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
